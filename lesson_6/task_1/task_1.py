206a611c69c270dbf7d5f5087b0a1adb9b99d703
Andriy-Hladkiy <andriyha98@gmail.com>
2019-07-21 17:06:58 +0300

    add lesson 5

--- lesson_6/task_1/task_1.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-with open("test1") as input_file:
-    count = Counter(word for line in input_file for word in line.split())
-    result = count.most_common(5)
-
-with open("test1_result.txt", "w") as result_file:
-    print(result, file=result_file)
--- lesson_6/task_2/test2.py
+++
@@ -1,7 +0,0 @@
-def count_squares(user_list):
-    list_of_squares = []
-    for n in user_list:
-        n = n**2
-        list_of_squares.append(n)
-    return sum(list_of_squares)
-
